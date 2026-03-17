53154c565a81f9a4145384c4f643429c1d9e3680
“Aumkar <aumkarwagle@gmail.comgit config --global user.name Aumkar Waglegit config --global user.email aumkarwagle@gmail.com>
2026-03-17 17:26:45 +0100

    Fix Groq request format

--- utils/llm_advisor.py
+++ utils/llm_advisor.py
@@ -10,7 +10,7 @@ def get_ai_response(prompt, api_key):
     client = Groq(api_key=api_key)
 
     response = client.chat.completions.create(
-        model="llama3-8b-8192",
+        model="llama3-70b-8192",
         messages=[
             {"role": "user", "content": str(prompt)}
         ],
